dc297f33e293b985f6ada7e0738f0d6b09e641d0
Tomasz Ż <tzuber95@gmail.com>
2024-01-31 19:59:25 +0100

    Create about me section on homepage

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,9 @@
-import SocialLinks from '../UI/SocialLinks'
-
 const Footer = () => {
 	const year = new Date().getFullYear()
 
 	return (
-		<footer className='container p-8 flex flex-col justify-center items-center gap-6 lg:gap-10 text-lg lg:text-xl'>
-			<SocialLinks />
-			<span className='text-center'>© Tomasz Żuber {year} | All Rights Reserved</span>
+		<footer className='container max-w-[1300px] p-8 text-lg lg:text-xl text-center'>
+			<span>© Tomasz Żuber {year} | All Rights Reserved</span>
 		</footer>
 	)
 }
