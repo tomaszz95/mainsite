c56b26f6721ffd435de8293421279da4533460d6
Tomasz Ż <tzuber95@gmail.com>
2024-02-01 21:10:07 +0100

    Create hightlight project section on homepage

--- src/components/homepageCards/CardItem.tsx
+++ src/components/homepageCards/CardItem.tsx
@@ -4,14 +4,14 @@ type ComponentType = { title: string; description: string; tech: string[]; link:
 
 const CardItem: React.FC<ComponentType> = ({ title, description, tech, link }) => {
 	return (
-		<div className='flex flex-col justify-between border border-hightlight even:border-hightlightSecond even:scale-110 border-1 max-w-[340px] w-full text-center rounded-3xl bg-main p-6 hover:scale-[0.98] even:hover:scale-[1.08] duration-300'>
+		<div className='flex flex-col justify-between border border-hightlight  even:scale-110 border-1 max-w-[340px] w-full text-center rounded-3xl bg-main p-6 hover:scale-[0.98] even:hover:scale-[1.08] duration-300'>
 			<div>
 				<h3 className='text-2xl mb-4 text-white'>{title}</h3>
 				<p className='mb-6'>{description}</p>
 			</div>
 			<ul className='flex flex-wrap justify-around gap-4 w-full'>
 				{tech.map(tech => (
-					<li key={tech} className='bg-slate-800 py-2 text-sm w-[45%] rounded-3xl'>
+					<li key={tech} className='bg-primary py-2 text-sm w-[45%] rounded-3xl'>
 						{tech}
 					</li>
 				))}
@@ -20,11 +20,7 @@ const CardItem: React.FC<ComponentType> = ({ title, description, tech, link }) =
 				target='_blank'
 				rel='noopener noreferrer'
 				href={link}
-				className={`${
-					title === 'Netflix clone'
-						? 'bg-hightlightSecond hover:bg-hightlightSecondHover'
-						: 'bg-hightlight hover:bg-hightlightHover'
-				} rounded-lg  duration-300 py-3 px-5 mt-6 w-48 mx-auto`}>
+				className='bg-hightlight hover:bg-hightlightHover rounded-xl  duration-300 py-3 px-5 mt-6 w-48 mx-auto'>
 				Check project site!
 			</Link>
 		</div>
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	const year = new Date().getFullYear()
 
 	return (
-		<footer className='container max-w-[1300px] p-8 text-lg lg:text-xl text-center'>
+		<footer className='w-full p-8 text-lg lg:text-xl text-center  border-t-2 border-t-main'>
 			<span>© Tomasz Żuber {year} | All Rights Reserved</span>
 		</footer>
 	)
