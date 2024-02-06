0f4ccf18f91147c084cbe1f0e409f43c5b74eea0
Tomasz Ż <tzuber95@gmail.com>
2024-02-06 21:06:52 +0100

    start contact section

--- src/components/contactViews/FastContactSection.tsx
+++ src/components/contactViews/FastContactSection.tsx
@@ -7,23 +7,34 @@ import EMAIL_ICON from '../../assets/icons/emailContact.png'
 import MOBILE_ICON from '../../assets/icons/mobileContact.png'
 
 const FastContactSection = () => {
+	const itemsClasses =
+		'flex flex-col justify-center items-center cursor-pointer border-2 p-4 m-5 w-full rounded-2xl hover:border-hightlight duration-300 gap-4 min-w-[220px] max-w-[300px]'
+
 	return (
-		<div>
-			<Link href='https://www.linkedin.com/in/tomasz-zuber95/' target='_blank' rel='noopener noreferrer'>
-				<Image src={LINKEDIN_ICON} alt='Linkedin icon' className='w-10 lg:w-11 hover:scale-110 duration-300' priority />
+		<div className='grid justify-items-center grid-cols-1 w-full'>
+			<Link
+				href='https://www.linkedin.com/in/tomasz-zuber95/'
+				target='_blank'
+				rel='noopener noreferrer'
+				className={itemsClasses}>
+				<Image src={LINKEDIN_ICON} alt='Linkedin icon' className='w-10 lg:w-11' priority />
 				<span>LinkedIn</span>
 			</Link>
-			<Link href='https://www.facebook.com/tomasz.zuber.315/' target='_blank' rel='noopener noreferrer'>
-				<Image src={FACEBOOK_ICON} alt='Facebook icon' className='w-10 lg:w-11 hover:scale-110 duration-300' priority />
+			<Link
+				href='https://www.facebook.com/tomasz.zuber.315/'
+				target='_blank'
+				rel='noopener noreferrer'
+				className={itemsClasses}>
+				<Image src={FACEBOOK_ICON} alt='Facebook icon' className='w-10 lg:w-11' priority />
 				<span>Facebook</span>
 			</Link>
-			<a href='mailto:tzuber95@gmail.com' target='blank' rel='noopener noreferrer'>
-				<Image src={EMAIL_ICON} alt='Email icon' className='w-10 lg:w-11 hover:scale-110 duration-300' priority />
-				<span>Email: tzuber95@gmail.com</span>
+			<a href='mailto:tzuber95@gmail.com' target='blank' rel='noopener noreferrer' className={itemsClasses}>
+				<Image src={EMAIL_ICON} alt='Email icon' className='w-10 lg:w-11' priority />
+				<span>tzuber95@gmail.com</span>
 			</a>
-			<div>
-				<Image src={MOBILE_ICON} alt='Mobile icon' className='w-10 lg:w-11 hover:scale-110 duration-300' priority />
-				<span>Mobile: +48 571 385 580</span>
+			<div className={itemsClasses}>
+				<Image src={MOBILE_ICON} alt='Mobile icon' className='w-10 lg:w-11' priority />
+				<span>+48 571 385 580</span>
 			</div>
 		</div>
 	)
