5de451e17fffcb2e3cb197a52ae8bfe886cc41a1
Tomasz Ż <tzuber95@gmail.com>
2024-02-09 21:23:46 +0100

    Create nextpc and netflix projects on project page

--- src/components/projectsViews/ProjectsListSection.tsx
+++ src/components/projectsViews/ProjectsListSection.tsx
@@ -1,5 +1,23 @@
+import { projectsList } from '../../constans/projects'
+import ProjectItem from '../projectItem/ProjectItem'
+
 const ProjectsListSection = () => {
-	return <div>ProjectsListSection</div>
+	return (
+		<ul className='w-full'>
+			{projectsList.map(proj => (
+				<li className='w-full flex flex-col items-center projectsList'>
+					<ProjectItem
+						title={proj.title}
+						key={proj.title}
+						description={proj.shortDesc}
+						stack={proj.technologies}
+						link={proj.link}
+						photo={proj.photos[0]}
+					/>
+				</li>
+			))}
+		</ul>
+	)
 }
 
 export default ProjectsListSection
