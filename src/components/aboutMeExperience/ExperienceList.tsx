166d114f1dbd445c76e7709fb95e452a76ededf9
Tomasz Ż <tzuber95@gmail.com>
2024-02-11 20:57:44 +0100

    Change image on about me page

--- src/components/aboutMeExperience/ExperienceList.tsx
+++ src/components/aboutMeExperience/ExperienceList.tsx
@@ -1,21 +1,27 @@
+import Image from 'next/image'
+
 import { lifeCareer } from '../../constans/aboutmeEducation'
+
 import ARROW_ICON from '../../assets/icons/arrowDown.png'
 import SCHOOL_ICON from '../../assets/icons/school.png'
 import WORK_ICON from '../../assets/icons/bag.png'
-import Image from 'next/image'
 
 const ExperienceList = () => {
 	return (
-		<ul className='flex flex-col justify-around items-stretch mt-8 w-full max-w-[500px]'>
+		<ul className='flex flex-col justify-around items-stretch mt-10 w-full max-w-[500px]'>
 			{lifeCareer.map((item, index) => (
 				<li key={item.description}>
 					<div className='text-center bg-main p-4 py-8 rounded-2xl relative'>
 						<h3 className='text-xl xl:text-2xl'>{item.name}</h3>
 						<i className='block my-1 xl:text-lg'>{item.date}</i>
 						<p className='font-bold xl:text-lg'>{item.description}</p>
-						<Image alt='' src={item.type === 'School' ? SCHOOL_ICON : WORK_ICON} className='absolute -top-6 right-2 w-12' />
+						<Image
+							alt=''
+							src={item.type === 'School' ? SCHOOL_ICON : WORK_ICON}
+							className='absolute -top-6 right-2 w-12'
+						/>
 					</div>
-					{index !== lifeCareer.length - 1 && <Image src={ARROW_ICON} alt='' className='mx-auto my-6' />}
+					{index !== lifeCareer.length - 1 && <Image src={ARROW_ICON} alt='' className='mx-auto my-5' />}
 				</li>
 			))}
 		</ul>
