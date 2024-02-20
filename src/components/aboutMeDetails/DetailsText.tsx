f5256167d8fd18e0bd64211c586051c929b4bbd1
Tomasz Ż <tzuber95@gmail.com>
2024-02-20 19:44:34 +0100

    Edit homepage photos

--- src/components/aboutMeDetails/DetailsText.tsx
+++ src/components/aboutMeDetails/DetailsText.tsx
@@ -2,17 +2,16 @@ const DetailsText = () => {
 	return (
 		<div className='my-4 max-w-[500px] lg:col-start-2 lg:col-end-3 text-center lg:text-[17px] lg:mt-0'>
 			<p className='my-6'>
-				My adventure with programming began more than two years ago. I started by learning Html, Css and Javascript
-				decently, to then enter the world of React and one of its more popular frameworks NextJs. I am currently
-				developing my knowledge in this area. In addition, I am planning to learn backend technologies as well - NodeJs,
-				Express, maybe Java. At the moment I am an aspiring Junior Frontend Developer, that wants to get his first job,
-				and to one day be called Fullstack Developer.
+				My programming journey began over two years ago. I started with HTML, CSS, and JavaScript, then ventured into
+				React and its popular framework Next.js. Currently, I'm expanding my knowledge in this area. Additionally, I
+				plan to learn backend technologies such as Node.js, Express, and possibly Java. At the moment I am an aspiring
+				Junior Frontend Developer eager to get my first job and ultimately become a Fullstack Developer someday.
 			</p>
 			<p>
-				I am disciplined, ambitious, and ready to continuously develop programming skills. I am interested in long-term
-				collaboration and work that allows me to improve and contribute significantly to the company. I am able to work
-				under pressure, both as a part of the team and independently when necessary. I enjoy problem-solving and
-				leveraging my creativity in work.
+				I am disciplined, ambitious, and ready to continuously improve my programming skills. I am interested in a
+				long-term collaboration and job that allows me to contribute significantly to the company while enhancing my
+				abilities. I can work under pressure, both as a part of the team and independently when necessary.
+				Problem-solving and leveraging creativity are aspects of work that I particularly enjoy.
 			</p>
 		</div>
 	)
--- src/components/homepageText/HomepageAboutText.tsx
+++ src/components/homepageText/HomepageAboutText.tsx
@@ -5,13 +5,12 @@ const HomepageAboutText = () => {
 	return (
 		<div className='p-2 max-w-lg lg:max-w-2xl flex flex-col'>
 			<div className='mb-16'>
-				<HeadingTwo text='Few words' coloredText='about me' />
+				<HeadingTwo text='A few words' coloredText='about me' />
 				<p className='lg:text-lg'>
-					What started as a curiosity and a desire to learn new technologies, quickly turned into my passion and
+					What began as mere curiosity and a desire to learn new technologies, quickly turned into my passion and
 					greatest hobby. Through systematic study and practice, I have acquired the skills needed to create fascinating
-					front-end projects, and there is still much to discover before me. Now, with more than two years of
-					programming experience, I am ready for new challenges and further development, ready to create inspiring and
-					engaging experiences.
+					front-end projects, yet there is still much to explore. Now, with over two years of programming experience, I
+					am ready for new challenges and continual growth, and eager to create inspiring and engaging experiences.
 				</p>
 			</div>
 			<LinkButton href='/about' openNewWindow={false}>
