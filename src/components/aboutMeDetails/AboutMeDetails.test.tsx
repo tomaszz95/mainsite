f5a17f943f27bb6c1ffb5c3a3d11015bd6a34ac3
Tomasz Ż <tzuber95@gmail.com>
2024-02-21 20:19:55 +0100

    Add testing packages and adapt them to project

---
+++ src/components/aboutMeDetails/AboutMeDetails.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import AboutMeDetails from './AboutMeDetails'
+
+describe('AboutMeDetails component', () => {
+	test('renders about me photo', () => {
+		render(<AboutMeDetails />)
+		const imageElement = screen.getByAltText('Programmer with dreadlocks profile photo')
+		expect(imageElement).toBeInTheDocument()
+	})
+})
