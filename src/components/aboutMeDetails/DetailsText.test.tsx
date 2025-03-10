77471405e8038ef3bdc7d7757ec6479a2a39948e
Tomasz Ż. <tzuber95@gmail.com>
2025-03-10 18:16:03 +0100

    Edit photos of single projects

--- src/components/aboutMeDetails/DetailsText.test.tsx
+++ src/components/aboutMeDetails/DetailsText.test.tsx
@@ -6,7 +6,7 @@ describe('DetailsText component', () => {
 	test('renders component with correct text content', () => {
 		const { getByText } = render(<DetailsText />)
 
-		expect(getByText(/My programming journey started over/i)).toBeInTheDocument()
+		expect(getByText(/My programming journey started nearly/i)).toBeInTheDocument()
 		expect(getByText(/I’m an aspiring Junior Frontend Developer/i)).toBeInTheDocument()
 	})
 })
