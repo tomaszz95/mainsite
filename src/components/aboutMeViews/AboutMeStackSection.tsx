968ce8d2644635a89536bdfd98bac1c554370505
Tomasz Ż. <tzuber95@gmail.com>
2025-04-22 18:16:30 +0200

    Change linked in link and aria label

--- src/components/aboutMeViews/AboutMeStackSection.tsx
+++ src/components/aboutMeViews/AboutMeStackSection.tsx
@@ -3,19 +3,26 @@ import CenteredSection from '../layout/CenteredSection'
 import StackList from '../UI/StackList'
 import HeadingTwo from '../UI/HeadingTwo'
 
-import { techStack, techToLearn } from '../../constans/aboutmeStack'
+import { techStack } from '../../constans/aboutmeStack'
 
 const AboutMeStackSection = () => {
 	return (
 		<Wrapper classes='evenSection'>
 			<CenteredSection>
-				<div>
-					<HeadingTwo text='Technology' coloredText='stack' />
-					<StackList stack={techStack} classes='lg:grid-cols-4 xl:grid-cols-6 xl:gap-x-0' />
-				</div>
-				<div className='mt-12 flex-col'>
-					<HeadingTwo text='Next to' coloredText='learn' />
-					<StackList stack={techToLearn} classes='lg:grid-cols-4 gap-x-20' />
+				<HeadingTwo text='Technology' coloredText='stack' />
+				<div className='grid grid-cols-12 gap-x-8 gap-y-12 lg:gap-x-20'>
+					<div className='col-span-12 lg:col-span-6'>
+						<h3>Frontend</h3>
+						<StackList stack={techStack.frontend} classes='xl:grid-cols-4' />
+					</div>
+					<div className='col-span-12 lg:col-span-6'>
+						<h3>Backend / Database</h3>
+						<StackList stack={techStack.backend} classes='xl:grid-cols-4' />
+					</div>
+					<div className='col-span-12'>
+						<h3>Other</h3>
+						<StackList stack={techStack.other} classes='lg:grid-cols-4 max-w-screen-md mx-auto' />
+					</div>
 				</div>
 			</CenteredSection>
 		</Wrapper>
--- src/constans/aboutmeStack.ts
+++ src/constans/aboutmeStack.ts
@@ -12,43 +12,38 @@ import GITHUB_ICON from '../assets/icons/github.png'
 import JEST_ICON from '../assets/icons/jest.png'
 import NODE_ICON from '../assets/icons/node.png'
 import SQL_ICON from '../assets/icons/sql.png'
-import VUE_ICON from '../assets/icons/vue.png'
 import RTL_ICON from '../assets/icons/ReactTL.png'
 import EXPRESS_ICON from '../assets/icons/express.png'
 import MONGOOSE_ICON from '../assets/icons/mongoose.png'
 import MYSQL_ICON from '../assets/icons/mysql.png'
 import POSTGRE_ICON from '../assets/icons/postgre.png'
-import WORDPRESS_ICON from '../assets/icons/wordpress.png'
 import GRAPHQL_ICON from '../assets/icons/graphql.png'
-import NESTJS_ICON from '../assets/icons/nestjs.png'
-import DOCKER_ICON from '../assets/icons/docker.png'
 
-export const techStack = [
-	{ name: 'React', icon: REACT_ICON },
-	{ name: 'Next.js', icon: NEXT_ICON },
-	{ name: 'Javascript', icon: JAVASCRIPT_ICON },
-	{ name: 'Typescript', icon: TYPESCRIPT_ICON },
-	{ name: 'Node.js', icon: NODE_ICON },
-	{ name: 'Express.js', icon: EXPRESS_ICON },
-	{ name: 'SQL', icon: SQL_ICON },
-	{ name: 'MySQL', icon: MYSQL_ICON },
-	{ name: 'PostgreSQL', icon: POSTGRE_ICON },
-	{ name: 'Mongoose', icon: MONGOOSE_ICON },
-	{ name: 'MongoDB', icon: MONGO_ICON },
-	{ name: 'Firebase', icon: FIREBASE_ICON },
-	{ name: 'JEST', icon: JEST_ICON },
-	{ name: 'React Testing Library', icon: RTL_ICON },
-	{ name: 'Wordpress', icon: WORDPRESS_ICON },
-	{ name: 'Tailwind', icon: TAILWIN_ICON },
-	{ name: 'Bootstrap', icon: BOOTSTRAP_ICON },
-	{ name: 'HTML', icon: HTML_ICON },
-	{ name: 'CSS', icon: CSS_ICON },
-	{ name: 'Github', icon: GITHUB_ICON },
-]
-
-export const techToLearn = [
-	{ name: 'GraphQL', icon: GRAPHQL_ICON },
-	{ name: 'Docker', icon: DOCKER_ICON },
-	{ name: 'Nest.js', icon: NESTJS_ICON },
-	{ name: 'VUE', icon: VUE_ICON },
-]
+export const techStack = {
+	frontend: [
+		{ name: 'React', icon: REACT_ICON },
+		{ name: 'Next.js', icon: NEXT_ICON },
+		{ name: 'Javascript', icon: JAVASCRIPT_ICON },
+		{ name: 'Typescript', icon: TYPESCRIPT_ICON },
+		{ name: 'HTML', icon: HTML_ICON },
+		{ name: 'CSS', icon: CSS_ICON },
+		{ name: 'Tailwind', icon: TAILWIN_ICON },
+		{ name: 'Bootstrap', icon: BOOTSTRAP_ICON },
+	],
+	backend: [
+		{ name: 'Node.js', icon: NODE_ICON },
+		{ name: 'Express.js', icon: EXPRESS_ICON },
+		{ name: 'MongoDB', icon: MONGO_ICON },
+		{ name: 'Mongoose', icon: MONGOOSE_ICON },
+		{ name: 'SQL', icon: SQL_ICON },
+		{ name: 'MySQL', icon: MYSQL_ICON },
+		{ name: 'PostgreSQL', icon: POSTGRE_ICON },
+		{ name: 'Firebase', icon: FIREBASE_ICON },
+	],
+	other: [
+		{ name: 'GraphQL', icon: GRAPHQL_ICON },
+		{ name: 'JEST', icon: JEST_ICON },
+		{ name: 'React Testing Library', icon: RTL_ICON },
+		{ name: 'Github', icon: GITHUB_ICON },
+	],
+}
